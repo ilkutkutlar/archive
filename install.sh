49104824fac054ebac3f091ec76a1b87ba211b52
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-07-19 15:08:15 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage: $(basename "$0") [install_dir]"
+  echo "install_dir defaults to /usr/local if omitted"
+  exit 0
+fi
+
+readonly PROJECT_ROOT="$(dirname "$0")"
+readonly INSTALL_DIR="${1-"/usr/local"}"
+
+install -d -m 755 "${INSTALL_DIR}/bin" "${INSTALL_DIR}/lib/archive"
+install -m 755 "${PROJECT_ROOT}/bin"/* "${INSTALL_DIR}/bin"
+install -m 755 "${PROJECT_ROOT}/lib"/* "${INSTALL_DIR}/lib/archive"
+
+echo "Installed archive"
--- lib/help.sh
+++ lib/help.sh
@@ -1,17 +1,17 @@
 usage() {
-  echo "Usage:"
-  echo "  $(basename "$0") -a file [-d]"
-  echo "  $(basename "$0") {-l|-v|-h}"
   echo "v0.0.1 - July 2020"
+  echo "Usage: $(basename "$0") {-a|-u} file [-d]"
+  echo "       $(basename "$0") {-l|-v|-h}"
 }
 
 help() {
   usage
   
   echo
-  echo '  -a    add file to archive of current directory'
-  echo '  -m    add file to archive and remove it'
-  echo '  -l    list the files in current directory archive'
-  echo '  -v    print version and exit'
-  echo '  -h    print this help and exit'
+  echo '  -a, --add FILE          Add file to archive of current directory'
+  echo '  -u, --unarchive FILE    Unarchive file from archive of current directory'
+  echo '  -d, --delete            Pass flag to -a or -u to delete file in dir/archive after operation'
+  echo '  -l, --list              List the files in current directory archive'
+  echo '  -v, --version           Print version and exit'
+  echo '  -h, --help              Print this help and exit'
 }
